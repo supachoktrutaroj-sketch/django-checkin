9b8082aa3d346ae725be3e4cf593611da0dd85b2
Jukkapun Muuta <jukkapun_muut@rtaf.mi.th>
2026-05-20 00:57:35 +0700

    add auto create superuser script

--- checkin/apps.py
+++ checkin/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class CheckinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkin'
+# โค้ดสร้างแอดมินอัตโนมัติเมื่อรันบนเซิร์ฟเวอร์จริง
+from django.contrib.auth.models import User
+try:
+    if not User.objects.filter(username='admin_railway').exists():
+        User.objects.create_superuser('admin_railway', 'admin@example.com', '12345678a')
+        print("✅ สร้างบัญชีแอดมินหลักสำเร็จแล้ว!")
+except Exception as e:
+    pass
